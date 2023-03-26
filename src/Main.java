261b05c68711af2232839b414daecd179fb109d8
David <ddlukula21@coe.edu>
2023-03-26 14:22:24 -0500

    Calculator is Done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("FAT Calculator");
+        JFrame frame = new JFrame("FAT CALCULATOR");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
